ba583db4c86379672a3c34b1787cef91894c51da
Geraint <luffgd@gmail.com>
2025-05-28 19:46:36 +0100

    Don't define `WasmP` until needed

--- source/wclap32/wclap-translation.h
+++ source/wclap32/wclap-translation.h
@@ -3,8 +3,6 @@
 #include "./translate-clap-structs.generated.h"
 
 #define WCLAP_MULTIPLE_INCLUDES_NAMESPACE wclap32
-#define WCLAP_MULTIPLE_INCLUDES_WASMP uint32_t
 #include "../wclapN/wclap-translation.h"
 #undef WCLAP_MULTIPLE_INCLUDES_NAMESPACE
-#undef WCLAP_MULTIPLE_INCLUDES_WASMP
 
--- source/wclap64/wclap-translation.h
+++ source/wclap64/wclap-translation.h
@@ -3,8 +3,6 @@
 #include "./translate-clap-structs.generated.h"
 
 #define WCLAP_MULTIPLE_INCLUDES_NAMESPACE wclap64
-#define WCLAP_MULTIPLE_INCLUDES_WASMP uint64_t
 #include "../wclapN/wclap-translation.h"
 #undef WCLAP_MULTIPLE_INCLUDES_NAMESPACE
-#undef WCLAP_MULTIPLE_INCLUDES_WASMP
 
